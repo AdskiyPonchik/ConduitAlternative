6856f8db36652a20aea3cf9c7d3e40e0e1f1ba3b
adskiyponchik <adskiyponchik@gmail.com>
2026-09-13 17:27:24 +0200

    Spring Setup

---
+++ src/test/java/de/conduit/ConduitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.conduit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConduitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
